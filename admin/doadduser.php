f3fcb177722be6e166b893a9a58609f52e1d19d7
chenhao <Iammrchen@outlook.com>
2018-10-31 16:34:51 +0800

    2018年10月31日16:31:44 用户中心 修改信息 上传头像 我的订单 完成界面修改

---
+++ admin/doadduser.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+  </body>
+</html>
+<?php
+include '../conn.php';
+//获取内容
+$username = $_REQUEST['username'];
+$password = $_REQUEST['password'];
+$tel      = $_REQUEST['tel'];
+$sex      = $_REQUEST['sex'];
+$email    = $_REQUEST['email'];
+$address  = $_REQUEST['address'];
+
+//写入数据库
+$sql = "insert into user(uname,pwd,tel,sex,email,address) values('".$username."','".$password."','".$tel."','".$sex."','".$email."','".$address."')";
+if ($conn->query($sql)==TRUE) {
+  echo "success！";
+    ?>
+    <script type="text/javascript">
+        //休眠5秒，跳转广告列表
+        $(function() {
+            sleep(500);
+            location.href="/admin/userlist.php";
+        });
+
+        function sleep(n) { //n表示的毫秒数
+            var start = new Date().getTime();
+            while (true) if (new Date().getTime() - start > n) break;
+        }
+    </script>
+    <?php
+}else {
+  echo "failed！";
+}
---
+++ js/dialog.js
@@ -0,0 +1,9 @@
+//显示弹框
+function showPopBox() {
+    document.getElementById('popBox-demo').style.display = 'block';
+}
+
+//关闭弹框
+function hidePopBox() {
+    document.getElementById('popBox-demo').style.display = 'none';
+}
